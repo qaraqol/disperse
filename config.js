1835c4202eb3373652d399b39771ba4c933d89d7
Amin <auwardak@gmail.com>
2024-12-14 19:10:27 +0430

    Update ReadMe

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ export const config = {
   rpcApi: "https://wax.qaraqol.com",
   privateKey: "",
   senderName: "",
-  contractName: "alien.worlds",
+  contractName: "alien.worlds", //eosio.token for WAX tokens
   tokenName: "TLM",
   tokenPrecision: 4, //Common include 8 for WAX and 4 for TLM
   memo: "Disperse",
